89a0c34366dc1a5360af65029bf6d76780c25694
Ernie Casilla <ecasilla@icloud.com>
2014-08-06 17:49:11 -0400

    inital package folders setup

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  //Project configuration.
+  grunt.initConfig({
+
+    jshint: {
+      all: ['{lib,tests,.}/*.js', 'bin/uncss'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    mochacov: {
+      unit: {
+        options: {
+          reporter: 'spec'
+        }
+      },
+      coverage: {
+        options: {
+          reporter: 'html-cov'
+        }
+      },
+      coveralls: {
+        options: {
+          coveralls: {
+            serviceName: 'travis-ci'
+          }
+        }
+      },
+      options: {
+        files: 'tests/*.js',
+        slow: 7500,
+        timeout: 20000
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-cov');
+
+  require('time-grunt')(grunt);
+
+  grunt.registerTask('cover', ['mochacov:coverage']);
+  grunt.registerTask('test', ['jshint', 'mochacov:unit']);
+  grunt.registerTask('travis', ['jshint', 'mochacov:unit', 'mochacov:coveralls']);
+  grunt.registerTask('default', 'test');
+};
+
